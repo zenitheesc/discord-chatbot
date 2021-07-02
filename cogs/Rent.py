e855a59ca6bc7adf9e7030c8fb463572b298e36f
nosf <tomefelipe0@usp.br>
2021-07-02 20:56:12 -0300

    <small debugs>

--- cogs/Rent.py
+++ cogs/Rent.py
@@ -13,14 +13,14 @@ class Rent(commands.Cog):
 
         client = pymongo.MongoClient(MONGODB_ATLAS_URI)
         self.db = client['discord-bot']['discord-bot']
-        self.rent = self.db.find_one({"description": "rent"})['rent']['rent']
-        self.time = self.db.find_one({"description": "rent"})['rent']['rent_time']
+        self.id = self.db.find_one({"description": "rent"})['discord_id']
+        self.time = self.db.find_one({"description": "rent"})['rent_time']
 
     #A function to register the rent of a beaglebone black for 2 hours
     @commands.command(
         brief='Uma função para alugar a Beaglebone Black de Maíra Canal (gerente do Alto Nível)',
         help='Use o comando >alugar para ter acesso irrestrito durante duas horas a uma Beaglebone Black. Caso alguém já esteja de posse dela, você será avisado/avisada',
-        aliases=['aluguel','aluga']
+        aliases=['aluguel','aluga','alugar']
     )
     async def alugar(self, ctx):
         await ctx.trigger_typing()
@@ -29,12 +29,12 @@ class Rent(commands.Cog):
 
         await reactToMessage(self.bot, ctx.message, [MESSAGE_EMOJI])
 
-        if not self.db.find_one({"description": "rent"})['rent']:
-            self.db.update({"description": "rent"},{"$set":{"rent": ctx.message.author.id}})
-            self.db.update({"description": "rent"},{"$set":{"rent_time": time()/3600}})
+        if self.id == 0:
+            self.db.update_one({"description": "rent"},{"$set":{"discord_id": ctx.message.author.id}})
+            self.db.update_one({"description": "rent"},{"$set":{"rent_time": time()/3600}})
             response = await ctx.send(f'A Beaglebone foi alugada com sucesso por `{ctx.author.mention}`, por duas horas a Beaglebone Black estará sobre sua posse')
         else:
-            response = await ctx.send(f'A Beaglebone já foi alugada por <@`{self.rent}`>, entre em contato com ela/ele ou aguarde o tempo de devolução')
+            response = await ctx.send(f'A Beaglebone já foi alugada por <@`{self.id}`>, entre em contato com ela/ele ou aguarde o tempo de devolução')
 
         await reactToResponse(self.bot, response, [MESSAGE_EMOJI])
 
@@ -51,10 +51,10 @@ class Rent(commands.Cog):
 
         await reactToMessage(self.bot, ctx.message, [MESSAGE_EMOJI])
 
-        if ctx.message.author.id != self.rent:
+        if ctx.message.author.id != self.id:
             response = await ctx.send(f'Você não está de posse da Beaglebone. Alugue usando `>alugar` caso ela esteja livre.')
         else:
-            self.db.update({"description": "rent"}, {"$set":{"rent": None}})
+            self.db.update_one({"description": "rent"}, {"$set":{"discord_id": 0}})
             response = await ctx.send(f'A Beaglebone foi devolvida. Obrigado `{ctx.author.mention}`!')
 
         await reactToResponse(self.bot, response, [MESSAGE_EMOJI])
@@ -63,10 +63,10 @@ class Rent(commands.Cog):
     @tasks.loop(minutes = 10)
     async def time_expired(self):
         if time()/3600 - self.time >= 2:
-            txt = (f'O tempo de uso da beaglebone expirou <@`{self.rent}`>. Alugue-a novamente caso não existam solicitações de aluguel anteriores.')
+            txt = (f'O tempo de uso da beaglebone expirou <@`{self.id}`>. Alugue-a novamente caso não existam solicitações de aluguel anteriores.')
             channel = await self.bot.fetch_channel(CHANNEL_ID)
             await channel.send(content=txt)
-            self.db.update({"description": "rent"}, {"$set":{"rent": None}})
+            self.db.update({"description": "rent"}, {"$set":{"discord_id": 0}})
 
 def setup(bot):
     bot.add_cog(Rent(bot))
