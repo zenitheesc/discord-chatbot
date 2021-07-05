30a995c2fe22cbd09436ac4b8cd7c5488ee07f06
nosf <tomefelipe0@usp.br>
2021-07-05 12:40:35 -0300

    changing the non-commands to non-command calls such as self.id

--- cogs/Rent.py
+++ cogs/Rent.py
@@ -30,11 +30,11 @@ class Rent(commands.Cog):
         await reactToMessage(self.bot, ctx.message, [MESSAGE_EMOJI])
 
         if not self.db.find_one({"description": "rent"}) or self.id == 0:
-            self.db.update_one({"description": "rent"}, {"$set":{"discord_id": ctx.message.author.id}}, upsert=True)
-            self.db.update_one({"description": "rent"}, {"$set":{"rent_time": time()/3600}}, upsert = True)
-            response = await ctx.send(f'A Beaglebone foi alugada com sucesso por `{ctx.author.mention}`, por duas horas a Beaglebone Black estará sobre sua posse')
+            self.db.update_one({"description": "rent"}, {"$set":{"discord_id": ctx.message.author.id}})
+            self.db.update_one({"description": "rent"}, {"$set":{"rent_time": time()/3600}})
+            response = await ctx.send(f'A Beaglebone foi alugada com sucesso por {ctx.author.mention}, por duas horas a Beaglebone Black estará sobre sua posse')
         else:
-            response = await ctx.send(f'A Beaglebone já foi alugada por <@`{self.id}`>, entre em contato com ela/ele ou aguarde o tempo de devolução')
+            response = await ctx.send(f'A Beaglebone já foi alugada por <@{self.id}>, entre em contato com ela/ele ou aguarde o tempo de devolução')
 
         await reactToResponse(self.bot, response, [MESSAGE_EMOJI])
 
@@ -55,7 +55,7 @@ class Rent(commands.Cog):
             response = await ctx.send(f'Você não está de posse da Beaglebone. Alugue usando `>alugar` caso ela esteja livre.')
         else:
             self.db.update_one({"description": "rent"}, {"$set":{"discord_id": 0}})
-            response = await ctx.send(f'A Beaglebone foi devolvida. Obrigado `{ctx.author.mention}`!')
+            response = await ctx.send(f'A Beaglebone foi devolvida. Obrigado {ctx.author.mention}!')
 
         await reactToResponse(self.bot, response, [MESSAGE_EMOJI])
 
@@ -64,7 +64,7 @@ class Rent(commands.Cog):
     async def time_expired(self):
         if time()/3600 - self.time >= 2:
             if self.id != 0:
-                txt = (f'O tempo de uso da beaglebone expirou <@`{self.id}`>. Alugue-a novamente caso não existam solicitações de aluguel anteriores.')
+                txt = (f'O tempo de uso da beaglebone expirou <@{self.id}>. Alugue-a novamente caso não existam solicitações de aluguel anteriores.')
                 channel = await self.bot.fetch_channel(CHANNEL_ID)
                 await channel.send(content=txt)
                 self.db.update({"description": "rent"}, {"$set":{"discord_id": 0}})
